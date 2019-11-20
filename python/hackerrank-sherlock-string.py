3192d3c5812c3a63c07baee8e9b96fe26d0693ec
John Postlewaite <p0stalweight@gmail.com>
2019-11-20 10:59:36 -0800

    applied autopep8

--- python/hackerrank-sherlock-string.py
+++ python/hackerrank-sherlock-string.py
@@ -7,6 +7,8 @@ import re
 import sys
 
 # Complete the isValid function below.
+
+
 def isValid(s):
     char_count = {}
 
@@ -18,8 +20,8 @@ def isValid(s):
             char_count[c] = 1
 
     # Verify all counts are the same, or with one exception
-    count_check = char_count[s[0]] #intialize to a value
-    char_removed = False 
+    count_check = char_count[s[0]]  # intialize to a value
+    char_removed = False
     for c in char_count:
         if char_count[c] == count_check:
             continue
@@ -31,8 +33,10 @@ def isValid(s):
             char_removed = True
         else:
             return "NO"
-        
+
     return "YES"
+
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
