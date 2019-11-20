eae00f1baefad954695e32f40501aaa8027a916c
p0stalweight <33821841+p0stalweight@users.noreply.github.com>
2019-11-20 10:57:45 -0800

    Create hackerrank-sherlock-string.py

---
+++ python/hackerrank-sherlock-string.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the isValid function below.
+def isValid(s):
+    char_count = {}
+
+    # Add all characters to hash table
+    for c in s:
+        if c in char_count:
+            char_count[c] += 1
+        else:
+            char_count[c] = 1
+
+    # Verify all counts are the same, or with one exception
+    count_check = char_count[s[0]] #intialize to a value
+    char_removed = False 
+    for c in char_count:
+        if char_count[c] == count_check:
+            continue
+        elif (char_count[c] == count_check + 1) and not char_removed:
+            char_removed = True
+        elif (char_count[c] == count_check - 1) and not char_removed:
+            char_removed = True
+        elif char_count[c] == 1 and not char_removed:
+            char_removed = True
+        else:
+            return "NO"
+        
+    return "YES"
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = isValid(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
